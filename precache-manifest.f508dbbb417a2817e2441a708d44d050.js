84dd5ba0779e0576769424969f6500a927d4456c
Rashed <rashedulislam8741@yahoo.com>
2019-09-13 12:45:18 +0600

    Updates

---
+++ precache-manifest.f508dbbb417a2817e2441a708d44d050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d2b1dc6839d04910f019297705858e",
+    "url": "/monstar-rolodex/index.html"
+  },
+  {
+    "revision": "587e86b31762a9148039",
+    "url": "/monstar-rolodex/static/css/main.6559963d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/monstar-rolodex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "587e86b31762a9148039",
+    "url": "/monstar-rolodex/static/js/main.ce248aa3.chunk.js"
+  },
+  {
+    "revision": "9e13696c89a7dc49e85a",
+    "url": "/monstar-rolodex/static/js/runtime~main.cfe6b30d.js"
+  }
+]);
